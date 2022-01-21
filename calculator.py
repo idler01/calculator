46d29c89458f1a92c561f017858fba1b39b03aad
idler01 <96712682+idler01@users.noreply.github.com>
2022-01-21 16:41:24 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,21 +16,45 @@ def mult (a,b):
 def div (a,b):
     res = (a/b)
     print (res)
-# user inputs
 
-a= int(input("write your first number: "))
-b= int(input("write your sec number:"))
-op = input("what do you want to do: ")
+# user inputs
+try :
+     a = float(input("write your first number: "))
+     op = input("what do you want to do: ")
+     b = float(input("write your sec number:"))
+
+except ZeroDivisionError :
+        print ("division by zero is not possible")
+except ValueError :
+        print ("try to use numbers")
+except Exception :
+        print ("something went wrong...")
+
+    #a= int(input("write your first number: "))
+    #b= int(input("write your sec number:"))
+    #op = input("what do you want to do: ")
+
 
 # results
+try :
+    if op=="+":
+        add (a,b)
+    elif op=="-":
+        sub (a,b)
+    elif op=="*":
+        mult (a,b)
+    elif op=="/":
+        div (a,b)
+    else :
+          print ("something went wrong")
 
-if op=="+":
-    add (a,b)
-elif op=="-":
-    sub (a,b)
-elif op=="*":
-    mult (a,b)
-elif op=="/":
-    div (a,b)
-else:
-    print("invalid user input")
+except ZeroDivisionError :
+    print ("division by zero is not possible")
+except ValueError :
+    print ("try to use numbers")
+except Exception :
+    print ("something went wrong...")
+
+finally :
+    print ("end")
+
